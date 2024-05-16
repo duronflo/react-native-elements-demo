03e03d2f2a3ef3e1e7bb04df971bf3c2ae0c636d
duronflo <flo@durons.net>
2024-05-17 00:26:47 +0200

    first version with react native elements

--- App.js
+++ App.js
@@ -1,12 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { Platform } from 'react-native';
+import { Button, lightColors, darkColors, Switch, createTheme, ThemeProvider, useThemeMode, useTheme } from '@rneui/themed';
+import { useState } from 'react';
+
+const theme = createTheme({
+  lightColors: {
+    primary: 'green',
+  },
+  darkColors: {
+    primary: 'green',
+  },
+  mode: 'light' | 'dark',
+  components: {
+    Button: {
+      raised: true,
+    },
+  },
+});
+
+
+const SwitchNew = () => {
+  const { mode, setMode } = useThemeMode();
+
   return (
-    <View style={styles.container}>
+    <Switch
+      value={() => mode === 'dark' ? false : true}
+      onValueChange={() => {
+
+        mode === 'dark' ? setMode('light') : setMode('dark');
+        console.log(mode)
+      }} />
+  )
+}
+
+const Main = () => {
+  const { theme } = useTheme();
+  return (
+    <View style={[styles.container, { backgroundColor: theme.colors.background }]}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="My Button" onPress={() => console.log(theme.colors)} />
+      <SwitchNew />
     </View>
+  )
+}
+
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Main />
+    </ThemeProvider>
   );
 }
 
@@ -18,3 +64,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
